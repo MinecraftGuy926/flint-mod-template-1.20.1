a4c9c464f8786e8fd616910bd950f91e11e0c9f3
Guy <gamerguy926@gmail.com>
2024-08-30 15:44:55 -0500

    initial commit

---
+++ src/main/java/net/mcguy/flint/FlintMod.java
@@ -0,0 +1,17 @@
+package net.mcguy.flint;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FlintMod implements ModInitializer {
+	public static final String MOD_ID = "flint_mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/mcguy/flint/FlintModClient.java
@@ -0,0 +1,10 @@
+package net.mcguy.flint;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FlintModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
